5de3424659cfe046abacf2b3be8075783c8e689f
Kitazawa Tomoya <42505878+tomoyaki@users.noreply.github.com>
2021-02-14 15:50:09 +0900

    for-roop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-document.getElementById("checkMagicWord").onclick = function () {
-  var magicWordEntered = document.getElementById("magicWord").value;
-  var magicWord = "abracatabra";
+/*
+for (var i = 5; i < 5; i++) {
+  console.log(i);
+}
 
-  if (magicWordEntered === magicWord) {
-    alert("OKです。");
-    var magicWordEntered = (document.getElementById("magicWord").value = "");
-  } else {
-    console.log("正しい暗号を入力してください。");
-    var magicWordEntered = (document.getElementById("magicWord").value = "");
-  }
-};
+for (var i = 5; i > 0; i--) {
+  console.log(i);
+}
+*/
+var tweets = ["Morning", "Good Day", "Dinner", "Good Night", "midnight"];
+var tweetString = "";
+
+for (var i = 0; i < tweets.length; i++) {
+  console.log(i);
+  console.log(tweetString);
+  tweetString = tweetString + "<p>" + tweets[i] + "</p>";
+
+  document.getElementById("tweetDiv").innerHTML = tweetString;
+}
