60eacb2e2f4f83ebc89ade321c658dfbf1a771c0
Kitazawa Tomoya <42505878+tomoyaki@users.noreply.github.com>
2021-02-14 15:54:16 +0900

    while-roop

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ for (var i = 5; i > 0; i--) {
 var tweets = ["Morning", "Good Day", "Dinner", "Good Night", "midnight"];
 var tweetString = "";
 
-for (var i = 0; i < tweets.length; i++) {
+//for (var i = 0; i < tweets.length; i++) {
+var i = 0;
+while (i < tweets.length) {
   console.log(i);
   console.log(tweetString);
   tweetString = tweetString + "<p>" + tweets[i] + "</p>";
+  i++;
 
   document.getElementById("tweetDiv").innerHTML = tweetString;
 }
