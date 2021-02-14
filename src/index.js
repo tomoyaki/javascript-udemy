e1fbb408007623220159a9c90ad3ea641be50fb9
Kitazawa Tomoya <42505878+tomoyaki@users.noreply.github.com>
2021-02-14 15:25:46 +0900

    if statement

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
-//foodの配列を作成
-var myArray = new Array();
-myArray[0] = "pizza";
-myArray[1] = "chocolate";
+document.getElementById("checkMagicWord").onclick = function () {
+  var magicWordEntered = document.getElementById("magicWord").value;
+  var magicWord = "abracatabra";
 
-//配列の要素数をカウント
-document.getElementById("countArray").onclick = function () {
-  alert(myArray.length);
+  if (magicWordEntered === magicWord) {
+    alert("OKです。");
+    var magicWordEntered = (document.getElementById("magicWord").value = "");
+  } else {
+    console.log("正しい暗号を入力してください。");
+    var magicWordEntered = (document.getElementById("magicWord").value = "");
+  }
 };
-//配列の内容を表示
-document.getElementById("showArray").onclick = function () {
-  alert(myArray);
-};
-
-console.log(myArray);
-
-//tweets配列
-var tweets = ["GoodMorning", "It's Lunch Time", "Almost Done!"];
-//配列の要素を追加
-tweets.push("Finish!");
-//要素の入れ替え
-tweets.splice(1, 0, "today's lunch is Pasta!");
-console.log(tweets);
